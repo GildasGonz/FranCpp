2c8bd87f8ee45d7c230ccd722a6157f32855baeb
Gildas Gonzalez <gildas.gonzalez@epitech.eu>
2021-03-12 17:36:29 +0100

    francpp-I: Adding version1 of francpp

---
+++ francpp.hpp
@@ -0,0 +1,60 @@
+/*
+** EPITECH PROJECT, 2021
+** francpp
+** File description:
+** francpp
+*/
+
+#include <iostream>
+
+#ifndef FRANCPP_HPP_
+#define FRANCPP_HPP_
+
+#define cdehors std::cout
+#define finl std::endl
+#define cerreur std::cerr
+#define cdedans std::cin
+#define vide void
+#define entier int
+#define flottant float
+#define double double
+#define booleen bool
+#define celuici this
+#define constante const
+#define caractere char
+#define phrase std::string
+#define retourne return
+#define classe class
+#define publique public
+#define prive private
+#define protege protected
+#define espacedenom namespace
+#define enumeration enum
+#define casse break
+#define cas case
+#define attrape catch
+#define essaye try
+#define vrai true
+#define faux false
+#define si if
+#define tantque while
+#define pour for
+#define fait do
+#define sinon else
+#define nouveau new
+#define operateur operator
+#define court short
+#define signe signed
+#define taillede sizeof
+#define statique static
+#define structure struct
+#define changeur switch
+#define modele template
+#define lancer throw
+#define definitiondutype typedef
+#define nonsigne unsigned
+#define raccord union
+#define virtuelle virtual
+#define surcharge override
+
+#endif /* !FRANCPP_HPP_ */
